83d6f5b747c1eaf002390adc2cb157f3bb702a09
Senix Code <71798807+senixcode@users.noreply.github.com>
2021-04-16 14:27:10 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./styles.css";
 import ScreenTestGridMe from "./pages/TestGridMe";
 import ScreenTestModalImage from "./pages/TestModalImage";
 import TestModal from "./modal-image/Index";
+import TestPhotoswipe from "./pages/TestPhotoswipe";
 export default function App() {
   return (
     <div className="App">
       <ScreenTestGridMe />
       <ScreenTestModalImage />
       <TestModal />
+      <TestPhotoswipe />
     </div>
   );
 }
