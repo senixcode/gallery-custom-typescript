015a9719e74c2304a1af2f45410c4164d3379046
Senix Code <71798807+senixcode@users.noreply.github.com>
2021-04-15 20:24:15 -0500

    add all

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./styles.css";
+// import "./styles_modal_image.css";
 import ScreenTestGridMe from "./pages/TestGridMe";
-
+import ScreenTestModalImage from "./pages/TestModalImage";
+import TestModal from "./modal-image/Index";
 export default function App() {
   return (
     <div className="App">
       <ScreenTestGridMe />
+      <ScreenTestModalImage />
+      <TestModal />
     </div>
   );
 }
---
+++ src/pages/TestModalImage.tsx
@@ -0,0 +1,9 @@
+import Test from "../modal-image/styles";
+export default function TestModalImage() {
+  return (
+    <>
+      <h1>Test Modal Image</h1>
+      <Test />
+    </>
+  );
+}
