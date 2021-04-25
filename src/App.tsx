6ab54496c302f8f1d405c28b307421dee193108a
Senix Code <71798807+senixcode@users.noreply.github.com>
2021-04-25 13:46:36 -0500

    style changes demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Basic from "./senixcode-lightbox-custom/examples/Basic";
 export default function App() {
   return (
     <ModalProvider>
-      <h1 style={{ textAlign: "center", marginTop: "1em" }}>
+      <h1 style={{ textAlign: "center", marginTop: "0.5em" }}>
         senixcode-lightbox-custom
       </h1>
       <h3 style={{ textAlign: "center" }}>typescript+css+style-components</h3>
       <h4 style={{ textAlign: "center" }}>
         features: masonry+gallery(image video)+modal, All custom
       </h4>
-      <h4 style={{ textAlign: "center", marginTop: "1em" }}>
+      <h4 style={{ textAlign: "center" }}>
         "No dynamic components, but custom"
       </h4>
 
